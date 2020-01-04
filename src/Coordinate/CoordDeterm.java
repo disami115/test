a203f824eb225a35d1070e81ff69068de955fec2
Sasha Miroshnichenko <disami115@yandex.ru>
2020-01-04 15:05:53 +0300

    add CoordDeterm

--- src/Coordinate/CoordDeterm.java
+++ src/Coordinate/CoordDeterm.java
@@ -1,5 +1,17 @@
 package Coordinate;
 
+import java.awt.MouseInfo;
+
 public class CoordDeterm {
-	
+	private int[] xy;
+	private int[] DetermXY()
+	{
+		xy['x'] = MouseInfo.getPointerInfo().getLocation().x;
+		xy['y'] = MouseInfo.getPointerInfo().getLocation().y;
+		return xy;
+	}
+	public int[] GetCoord()
+	{
+		return DetermXY();
+	}
 }
