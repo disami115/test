c7df67fa5e7c41f9ef5bec377b140327b5e47833
Sasha <disami115@yandex.ru>
2020-01-16 16:35:43 +0300

    Стрелку сделал)))

--- src/MouseDraw/DrawObjectArrow.java
+++ src/MouseDraw/DrawObjectArrow.java
@@ -3,11 +3,15 @@ package MouseDraw;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 public class DrawObjectArrow extends DrawObject {
 
+	private int x, y;
+	
 	public DrawObjectArrow(Graphics g, MouseEvent e) {
 		super(g, e);
 		// TODO Auto-generated constructor stub
@@ -15,30 +19,39 @@ public class DrawObjectArrow extends DrawObject {
 
 	@Override
 	public Graphics Draws(int x1, int y1, int x2, int y2, Color c) {
+		x = x1;
+		y = y1;
 		Graphics2D g = (Graphics2D) this.g;
-		Rectangle2D r2 = new Rectangle2D.Double(r.x, r.y, r.width, Math.sqrt(x1 * x1 + x2 * x2));
-		g.setColor(Color.blue);
-		double a;
-		double b;
-		double h;
-		double cx1,cy1;
-		//g.rotate(135*Math.PI/180);
-		//g.draw(r2);
-		a = Math.sqrt(r.width * r.width + r.height * r.height);
-		//b = Math.sqrt(a*a + 100 - 141.42 * a);
-		h = Math.sqrt(50);
-		cx1 =  (x2+h);
-		cy1 =  (y2+(a-h));
-		System.out.println(cx1 + " " + cy1);
-		g.drawLine(x1, y1, x2, y2);
 		g.setColor(Color.black);
-		g.drawLine(x2, y2, (int)cx1, (int)cy1);
-		//g.drawRect(r.x, r.y, Math.sqrt(x1 * x1 + x2 * x2), r.height);
-		//g.fillOval(e.getX(), e.getY(), 10, 10);
+		rotate(g, 10, x2, y2);
+		findPoint(x, y, x2, y2);
+		g.drawLine(x, y, x2, y2);
+		rotate(g,-20, x2, y2);
+		g.drawLine(x, y, x2, y2);
+		rotate(g, 10, x2, y2);
+		g.drawLine(x1, y1, x2, y2);
 		g.dispose();
 		return g;
 	}
-
+	
+	public void rotate (Graphics2D g, int deg, int x1, int y1){
+		g.rotate(Math.toRadians(deg), x1, y1);
+	}
+	
+	public void findPoint(int x, int y, int x2, int y2) {
+		this.x = (int) ((x + 3*x2)/4);
+		this.y = (int) ((y + 3*y2)/4);
+	}
+	
+public Shape drawArrow(int x1, int y1) {
+	Shape s = null;
+	s.contains(new Rectangle2D.Double(x1,y1,10,40));
+	return s;
+	
+	
+}
 
 
 }
+
+
